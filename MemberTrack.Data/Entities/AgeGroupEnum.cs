c9dc9a74b8c2588e17bd679786a9024b313362a6
AlbertoD <AlbertoD@ULTRABOOK-1404>
2017-02-20 14:42:20 -0600

    Fully qualify enums

--- MemberTrack.Data/Entities/AgeGroupEnum.cs
+++ MemberTrack.Data/Entities/AgeGroupEnum.cs
@@ -4,11 +4,11 @@ namespace MemberTrack.Data.Entities
 {
     public enum AgeGroupEnum
     {
-        [Description("Unknown")] Unknown = 1,
-        [Description("18 - 29")] Group1,
-        [Description("29 - 39")] Group2,
-        [Description("40 - 50")] Group3,
-        [Description("50 - 59")] Group4,
-        [Description("60+")] Group5
+        [Description("Unknown")] Unknown = 0,
+        [Description("18 - 29")] Group1 = 1,
+        [Description("29 - 39")] Group2 = 2,
+        [Description("40 - 50")] Group3 = 3,
+        [Description("50 - 59")] Group4 = 4,
+        [Description("60+")] Group5 = 5
     }
 }
--- MemberTrack.Data/Entities/CheckListItemTypeEnum.cs
+++ MemberTrack.Data/Entities/CheckListItemTypeEnum.cs
@@ -4,14 +4,14 @@ namespace MemberTrack.Data.Entities
 {
     public enum CheckListItemTypeEnum
     {
-        [Description("Unknown")] Unknown = 1,
-        [Description("1st Visit")] FirstVisit,
-        [Description("2nd Visit")] SecondVisit,
-        [Description("3rd Visit")] ThirdVisit,
-        [Description("4th Visit")] FourthVisit,
-        [Description("Membership Request Date")] MembershipRequestDate,
-        [Description("Membership Announcement Date")] MembershipAnnouncementDate,
-        [Description("Life Group")] LifeGroup,
-        [Description("Ministry")] Ministry
+        [Description("Unknown")] Unknown = 0,
+        [Description("1st Visit")] FirstVisit = 1,
+        [Description("2nd Visit")] SecondVisit = 2,
+        [Description("3rd Visit")] ThirdVisit = 3,
+        [Description("4th Visit")] FourthVisit = 4,
+        [Description("Membership Request Date")] MembershipRequestDate = 5,
+        [Description("Membership Announcement Date")] MembershipAnnouncementDate = 6,
+        [Description("Life Group")] LifeGroup = 7,
+        [Description("Ministry")] Ministry = 8
     }
 }
