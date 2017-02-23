1a1bbf18ddc10b55bdfad125670ddda4551cc16f
David <david.maltby@hotmail.com>
2017-02-23 08:39:06 -0600

    Allow for more than one SystemAccountHelper.  (This also introduces unit tests to the solution)

--- Tests/MemberTrack.Data.Tests/SystemAccountHelperTests.cs
+++ Tests/MemberTrack.Data.Tests/SystemAccountHelperTests.cs
@@ -16,5 +16,49 @@ namespace MemberTrack.Data.Tests
             Assert.IsTrue(systemAccounts.All(accounts => accounts.Role == Entities.UserRoleEnum.SystemAdmin));
         }
 
+        [TestMethod]
+        public void IsSystemAccount_ID_Success()
+        {
+            var systemAccounts = SystemAccountHelper.SystemAccounts;
+            
+            foreach (var systemAccount in systemAccounts)
+            {
+                var isSystemAccount = SystemAccountHelper.IsSystemAccount(systemAccount.Id);
+                Assert.IsTrue(isSystemAccount);
+            }
+        }
+
+        [TestMethod]
+        public void IsSystemAccount_Email_Success()
+        {
+            var systemAccounts = SystemAccountHelper.SystemAccounts;
+
+            foreach (var systemAccount in systemAccounts)
+            {
+                var isSystemAccount = SystemAccountHelper.IsSystemAccount(systemAccount.Email);
+                Assert.IsTrue(isSystemAccount);
+            }
+        }
+
+        [TestMethod]
+        public void IsSystemAccount_ID_Failure()
+        {
+            //Simply test that a few IDs that would be IDENTITY values in the database's User.Id column
+            //are NOT system accounts.
+            for (int counter = 1; counter < 10; counter++)
+            {
+                Assert.IsFalse(SystemAccountHelper.IsSystemAccount(counter));
+            }
+        }
+
+        [TestMethod]
+        public void IsSystemAccount_Email_Failure()
+        {
+            Assert.IsFalse(SystemAccountHelper.IsSystemAccount("joe.blow@nowhere.com"));
+            Assert.IsFalse(SystemAccountHelper.IsSystemAccount("remember.the.alamo@texas.independence.com"));
+            Assert.IsFalse(SystemAccountHelper.IsSystemAccount("warsteiner.dunkel@wurstfest.com"));
+            Assert.IsFalse(SystemAccountHelper.IsSystemAccount("Jesus.Wept@John1135.com"));
+        }
+
     }
 }
