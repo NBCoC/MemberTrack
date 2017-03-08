922cc220d4efa5e09322a5ac962814c771cf4e25
AlbertoD <AlbertoD@ULTRABOOK-1404>
2017-03-08 14:38:34 -0600

    Issue #6 allow for a person to be deleted

--- MemberTrack.Client/src/view-models/member-details.view-model.ts
+++ MemberTrack.Client/src/view-models/member-details.view-model.ts
@@ -1,21 +1,26 @@
-import { PersonEvent } from '../core/custom-events';
+import { PersonEvent, PromptEvent } from '../core/custom-events';
 import { autoinject } from 'aurelia-framework';
 import { BaseViewModel } from '../core/base-view-model';
 import { PersonService } from '../services/person.service';
 import { AuthService } from '../services/auth.service';
 import { PersonDto } from '../core/dtos';
+import { PromptDialogViewModel } from './prompt-dialog.view-model';
+import { Router } from 'aurelia-router';
 
 @autoinject
 export class MemberDetailsViewModel extends BaseViewModel {
     private personService: PersonService;
     private authService: AuthService;
+    private router: Router;
+    public promptDialogVm: PromptDialogViewModel;
     public person: PersonDto;
     public isEditor: boolean;
 
-    constructor(personService: PersonService, authService: AuthService) {
+    constructor(personService: PersonService, authService: AuthService, router: Router) {
         super('member-details');
         this.personService = personService;
         this.authService = authService;
+        this.router = router;
         this.initDefaultModel();
         this.isEditor = false;
     }
@@ -48,6 +53,27 @@ export class MemberDetailsViewModel extends BaseViewModel {
         let event = e.detail.args as PersonEvent;
 
         this.refreshModel(event.data);
+    }
+
+    public displayPromptDialog(): void {
+        if (!this.isEditor) {
+            return;
+        }
+        let message = 'Are you sure want to delete this member / visitor? Note that this cannot be undone!';
+        let name = this.person.firstName + ' ' + this.person.lastName;
+
+        this.promptDialogVm.show('Delete', message, name, this.person.id);
+    }
+
+    public dismissPromptDialog(e: CustomEvent): void {
+        let event = e.detail.args as PromptEvent;
+
+        this.personService.remove(event.data).then(ok => {
+            if (!ok) {
+                return;
+            }
+            this.router.navigateToRoute('home');
+        });
     }
 
     private initDefaultModel(): void {
